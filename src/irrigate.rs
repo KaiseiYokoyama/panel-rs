efc5f7e0fa11799b56f66765897b8310d0227bd7
Unknown <crome110mine@gmail.com>
2019-08-03 13:36:56 +0900

    fix while -> loop

--- src/irrigate.rs
+++ src/irrigate.rs
@@ -58,19 +58,21 @@ impl Irrigater {
         let y = self.zero_point.1;
         if let Ok(pixel) = self.get_pixel(x, y) {
             self.queue.push_back((x, y));
-            while !self.queue.is_empty() {
+            loop {
                 self.flood_fill_step();
+                if !self.queue.is_empty() {
+                    break;
+                }
             }
-//            for i in 0..150000 {
+//            while !self.queue.is_empty() {
 //                self.flood_fill_step();
-//                if self.queue.is_empty() { break; }
 //            }
         }
     }
 
     fn flood_fill_step(&mut self) {
         if let Some((x, y)) = self.queue.pop_front() {
-            println!("flood_fill_step: ({},{})", x, y);
+//            println!("flood_fill_step: ({},{})", x, y);
             if let Ok(pixel) = self.get_pixel(x, y) {
                 if self.image_table[y as usize][x as usize].is_none() && judge(&pixel, &self.reference_value, self.color_tolerance) {
                     self.image_table[y as usize][x as usize] = Some(Flag::Flame);
@@ -98,7 +100,7 @@ impl Irrigater {
                     }
                 }
             }
-            println!("queue: {:?}", &self.queue);
+//            println!("queue: {:?}", &self.queue);
         }
     }
 
@@ -117,89 +119,21 @@ impl Irrigater {
 }
 
 pub fn irrigate(image: &mut DynamicImage, color_tolerance: u32, zero_point: (u32, u32)) -> PanelResult<()> {
-    // 基準色
-//    let standard = if zero_point.0 > img.width() || zero_point.1 > img.height() {
-//        return Err(PanelError::RangeError(format!("image: [{},{}], zero_point: ({},{})", img.width(), img.height(), zero_point.0, zero_point.1)));
-//    } else { img.get_pixel(zero_point.0, zero_point.1).clone() };
-
-//    let mut map = [[None; img.width()]; img.height()];
-//    let mut table: Vec<Vec<Option<Flag>>> = Vec::with_capacity(img.height() as usize);
-//    for i in 0..img.height() {
-//        let mut row = Vec::with_capacity(img.width() as usize);
-//        for i in 0..img.width() {
-//            row.push(None);
-//        }
-//        table.push(row);
-//    }
-
-//    search_flame(&img, &mut table, &standard, color_tolerance, zero_point.0 as usize, zero_point.1 as usize);
-
     let mut irrigater = Irrigater::new(&image, color_tolerance, zero_point)?;
     irrigater.flood_fill();
 
-
     // todo remove
-    let mut img = image.to_rgba();
-    let red = Rgba::from_channels(255, 0, 0, 255);
-    for i in 0..img.height() {
-        for j in 0..img.width() {
-            if irrigater.image_table[i as usize][j as usize] == Some(Flag::Flame) {
-                img.put_pixel(j, i, red.clone());
-            }
-        }
-    }
-
-    img.save("panels-irrigated.png").unwrap();
+//    let mut img = image.to_rgba();
+//    let red = Rgba::from_channels(255, 0, 0, 255);
+//    for i in 0..img.height() {
+//        for j in 0..img.width() {
+//            if irrigater.image_table[i as usize][j as usize] == Some(Flag::Flame) {
+//                img.put_pixel(j, i, red.clone());
+//            }
+//        }
+//    }
+//
+//    img.save("panels-irrigated.png").unwrap();
 
     Ok(())
-}
-
-fn search_flame(img: &ImageBuffer<Rgba<u8>, Vec<u8>>, table: &mut Vec<Vec<Option<Flag>>>, standard: &Rgba<u8>, color_tolerance: u32, x: usize, y: usize) {
-    if table[y][x].is_some() { return; }
-
-    let pixel = img.get_pixel(x as u32, y as u32);
-
-    dbg!(x);
-
-    if judge(pixel, &standard, color_tolerance) {
-        table[y][x] = Some(Flag::Flame);
-
-        // 上下左右を見る
-        if match table.get(y + 1) {
-            None => false,
-            Some(row) => match row.get(x) {
-                None => false,
-                Some(_) => true,
-            }
-        } {
-            search_flame(img, table, standard, color_tolerance, x, y + 1);
-        }
-        if match table.get(y - 1) {
-            None => false,
-            Some(row) => match row.get(x) {
-                None => false,
-                Some(_) => true,
-            }
-        } {
-            search_flame(img, table, standard, color_tolerance, x, y - 1);
-        }
-        if match table.get(y) {
-            None => false,
-            Some(row) => match row.get(x - 1) {
-                None => false,
-                Some(_) => true,
-            }
-        } {
-            search_flame(img, table, standard, color_tolerance, x - 1, y);
-        }
-        if match table.get(y) {
-            None => false,
-            Some(row) => match row.get(x + 1) {
-                None => false,
-                Some(_) => true,
-            }
-        } {
-            search_flame(img, table, standard, color_tolerance, x + 1, y);
-        }
-    }
 }
